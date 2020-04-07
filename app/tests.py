79b3cb2c9a1704919691b851f1423ca4f9ca04ab
Tom <abaotom14@gmail.com>
2020-04-07 23:22:00 +0800

    add post test cases

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,9 @@
+import json
+
 from django.test import TestCase
 
 from app.models import Messages
+
 
 class MessagesModelTest(TestCase):
     title = 'Big'
@@ -18,7 +21,19 @@ class MessagesModelTest(TestCase):
         message = Messages.objects.get(id=1)
         self.assertEquals(message.message, self.message)
 
+
 class MessagesViewTest(TestCase):
+    title = 'Big'
+    message = 'Bob'
+
+    @classmethod
+    def setUpTestData(self):
+        self.data = {
+            'title': self.title,
+            'message': self.message
+        }
+
+
     def test_get_agilemanifesto(self):
         response = self.client.get('/agilemanifesto')
         self.assertEqual(response.status_code, 200)
@@ -29,6 +44,20 @@ class MessagesViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
+    def test_post_agilemanifesto_values(self):
+        response = self.client.post('/agilemanifesto/values',
+            json.dumps(self.data),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+
     def test_get_agilemanifesto_principles(self):
         response = self.client.get('/agilemanifesto/principles')
         self.assertEqual(response.status_code, 200)
+
+
+    def test_post_agilemanifesto_principles(self):
+        response = self.client.post('/agilemanifesto/principles',
+            json.dumps(self.data),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 201)

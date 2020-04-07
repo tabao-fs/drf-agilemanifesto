5ebef08958614344e2d476017533b0aafe94bc53
Tom <abaotom14@gmail.com>
2020-04-07 22:02:23 +0800

    edit serializers to match choices and add create

--- app/models.py
+++ app/models.py
@@ -3,11 +3,12 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
-class Messages(models.Model):
+class TextType(models.TextChoices):
+    VALUES = 'VAL', _('Values')
+    PRINCIPLES = 'PRN', _('Principles')
 
-    class TextType(models.TextChoices):
-        VALUES = 'VAL', _('Values')
-        PRINCIPLES = 'PRN', _('Principles')
+
+class Messages(models.Model):
 
     title = models.CharField(max_length=256)
     text_type = models.CharField(
--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from snippets.models import Messages
+
+from app.models import Messages, TextType
 
 
 class MessagesSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=256)
-    messages = serializers.TextField()
+    text_type = serializers.ChoiceField(choices=TextType.choices)
+    message = serializers.CharField(style={'base_template': 'textarea.html'})
+
+    def create(self, validated_data):
+        return Messages.objects.create(**validated_data)

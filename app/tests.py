b96d8690bff378f24ca29f9c8acab9032b6e6d7f
Tom <abaotom14@gmail.com>
2020-04-07 22:33:01 +0800

    add MessagesModelTest and MessagesViewTest test classes

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app.models import Messages
+
+class MessagesModelTest(TestCase):
+    title = 'Big'
+    message = 'Bob'
+
+    @classmethod
+    def setUpTestData(self):
+        Messages.objects.create(title=self.title, message=self.message)
+
+    def test_title(self):
+        message = Messages.objects.get(id=1)
+        self.assertEquals(message.title, self.title)
+
+    def test_message(self):
+        message = Messages.objects.get(id=1)
+        self.assertEquals(message.message, self.message)
+
+class MessagesViewTest(TestCase):
+    def test_get_agilemanifesto(self):
+        response = self.client.get('/agilemanifesto')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_get_agilemanifesto_values(self):
+        response = self.client.get('/agilemanifesto/values')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_get_agilemanifesto_principles(self):
+        response = self.client.get('/agilemanifesto/principles')
+        self.assertEqual(response.status_code, 200)

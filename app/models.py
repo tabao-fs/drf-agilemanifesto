3d7c94e5cb538dce139884df73a6279ee496fd1f
Tom <abaotom14@gmail.com>
2020-04-07 21:14:08 +0800

    add Messages model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+from django.utils.translation import gettext_lazy as _
+
 from django.db import models
 
-# Create your models here.
+
+class Messages(models.Model):
+
+    class TextType(models.TextChoices):
+        VALUES = 'VAL', _('Values')
+        PRINCIPLES = 'PRN', _('Principles')
+
+    title = models.CharField(max_length=256)
+    text_type = models.CharField(
+        max_length=3,
+        choices=TextType.choices,
+        default=TextType.VALUES,
+    )
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
